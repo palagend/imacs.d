827e3b1c3b219a9c4e7ec3778641db881155d378
Grain Stark <palagend@gmail.com>
2018-05-30 16:56:41 +0800

    Prepare for C/C++ IDE

--- opt-init.el
+++ opt-init.el
@@ -4,40 +4,23 @@
    '(
      treemacs-projectile
      helm-projectile
-     guide-key
-     visual-regexp
-     fill-column-indicator
+     company
      flycheck
      flycheck-pos-tip
-     flycheck-clojure
      yasnippet
      smartparens
      magit
      paredit
+     multiple-cursors
      move-text
-     prodigy
-     htmlize
      markdown-mode
-     nodejs-repl
      restclient
-     highlight-escape-sequences
-     whitespace-cleanup-mode
-     elisp-slime-nav
      dockerfile-mode
-     clojure-mode
-     clojure-mode-extra-font-locking
-     groovy-mode
-     prodigy
-     cider
-     yesql-ghosts
      string-edit
      beginend
-     expand-region
-     multiple-cursors
-     jump-char
-     multifiles
-     change-inner
-     browse-kill-ring
+     guide-key
+     js2-mode
+     js2-refactor
      )))
 
 (condition-case nil
@@ -46,10 +29,6 @@
    (package-refresh-contents)
    (init--install-packages)))
 
-;; Setup environment variables from the user's shell.
-(when is-mac
-  (require-package 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
 
 ;; guide-key
 (require 'guide-key)
@@ -67,9 +46,6 @@
 (require 'setup-paredit)
 (beginend-global-mode)
 
-(require 'prodigy)
-(global-set-key (kbd "C-x M-m") 'prodigy)
-
 ;; Default setup of smartparens
 (require 'smartparens-config)
 (setq sp-autoescape-string-quote nil)
@@ -86,27 +62,12 @@
 ;; Language specific setup files
 (eval-after-load 'js2-mode '(require 'setup-js2-mode))
 (eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
-(eval-after-load 'clojure-mode '(require 'setup-clojure-mode))
 (eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
 
-;; Load stuff on demand
-(autoload 'skewer-start "setup-skewer" nil t)
-(autoload 'skewer-demo "setup-skewer" nil t)
-(autoload 'auto-complete-mode "auto-complete" nil t)
 (eval-after-load 'flycheck '(require 'setup-flycheck))
 
 ;; Map files to modes
 (require 'mode-mappings)
-
-;; Highlight escape sequences
-(require 'highlight-escape-sequences)
-(hes-mode)
-(put 'font-lock-regexp-grouping-backslash 'face-alias 'font-lock-builtin-face)
-
-;; Visual regexp
-(require 'visual-regexp)
-(define-key global-map (kbd "M-&") 'vr/query-replace)
-(define-key global-map (kbd "M-/") 'vr/replace)
 
 ;; Functions (load all files in defuns-dir)
 (setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
@@ -114,49 +75,12 @@
   (when (file-regular-p file)
     (load file)))
 
-(require 'expand-region)
 (require 'multiple-cursors)
-(require 'delsel)
-(require 'jump-char)
-(require 'change-inner)
-(require 'multifiles)
-
-;; Don't use expand-region fast keys
-(setq expand-region-fast-keys-enabled nil)
-
-;; Show expand-region command used
-(setq er--show-expansion-message t)
-
-;; Fill column indicator
-(require 'fill-column-indicator)
-(setq fci-rule-color "#111122")
-
-;; Browse kill ring
-(require 'browse-kill-ring)
-(setq browse-kill-ring-quit-action 'save-and-restore)
 
 ;; Setup key bindings
 (require 'key-bindings)
 
-;; Misc
-(require 'my-misc)
-(when is-mac (require 'mac))
-
-;; Elisp go-to-definition with M-. and back again with M-,
-(autoload 'elisp-slime-nav-mode "elisp-slime-nav")
-(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode 1)))
-
-;; Emacs server
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-
-;; Run at full power please
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-
-;; Conclude init by setting up specifics for the current user
+  ;; Conclude init by setting up specifics for the current user
 (when (file-exists-p user-settings-dir)
   (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
 
--- users/huyh/ex-key-bindings.el
+++ users/huyh/ex-key-bindings.el
@@ -2,3 +2,5 @@
 (global-set-key (kbd "M-<return>") 'toggle-frame-fullscreen)
 
 (global-set-key (kbd "C-S-f") 'grep-find)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
