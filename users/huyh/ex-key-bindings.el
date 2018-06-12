044a2e911e4de1a9a7778edf5094b6305c8b7653
Grain Stark <palagend@gmail.com>
2018-06-12 17:09:55 +0800

    Define custome key-bindings

--- users/huyh/ex-key-bindings.el
+++
@@ -1,6 +0,0 @@
-;; Toggle full screen
-(global-set-key (kbd "M-<return>") 'toggle-frame-fullscreen)
-
-(global-set-key (kbd "C-S-f") 'grep-find)
-
-(global-set-key (kbd "M-x") 'helm-M-x)
--- users/palagend/ex-key-bindings.el
+++ users/palagend/ex-key-bindings.el
@@ -1,4 +1,11 @@
 ;; Toggle full screen
 (global-set-key (kbd "M-<return>") 'toggle-frame-fullscreen)
 
+(global-set-key (kbd "C-S-f") 'grep-find)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+(global-set-key (kbd "C-c p p") 'treemacs)
+
+(global-set-key (kbd "C-c p f") 'helm-projectile-find-file)
 
